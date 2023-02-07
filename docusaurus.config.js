7efc35fca44e609f043816e81838d6127282926e
Fulvio Corno <fulvio.corno@polito.it>
2023-02-07 14:34:29 +0100

    deployment scripts
    copied from https://docusaurus.io/docs/deployment#triggering-deployment-with-github-actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'e-Lite Research Group',
   tagline: 'e-Lite: Intelligent and Interactive Systems',
-  url: 'https://elite.polito.it/',
+
+  url: 'https://elite-polito.github.io/',
+
+  // GitHub
+  projectName: 'elite-polito.github.io',
+  organizationName: 'elite-polito',
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
+
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  // organizationName: 'facebook', // Usually your GitHub org/user name.
-  // projectName: 'docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
