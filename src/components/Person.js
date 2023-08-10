6b184166af5c195fa63828beec267e802c715a31
Fulvio Corno <fulvio.corno@polito.it>
2023-08-10 09:34:59 +0200

    Updated and completed the 'Alumni' section.
    Fixes #1

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '@fortawesome/free-solid-svg-icons'
-import {faGithub, faTelegram, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faTelegram, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 
 export const Person = (props) => (
@@ -12,13 +12,13 @@ export const Person = (props) => (
             <div className="card__body">
                 <div className="avatar avatar--vertical">
                     <img className="avatar__photo avatar__photo--xl"
-                         src={props.img || "/img/people/VOID.webp"}
-                         alt={'Picture of '+props.name}
+                        src={props.img || "/img/people/VOID.webp"}
+                        alt={'Picture of ' + props.name}
                     />
                     <div className="avatar__intro">
                         <div className="avatar__name">{props.name}</div>
-                        <small className="avatar__subtitle">{props.role}<br/>
-                            <address><a href={"mailto:" + props.email}>{props.email}</a><br/></address>
+                        <small className="avatar__subtitle">{props.role}<br />
+                            {props.email && <address><a href={"mailto:" + props.email}>{props.email}</a><br /></address>}
                         </small>
                     </div>
                 </div>
@@ -28,13 +28,16 @@ export const Person = (props) => (
 
                 {props.github &&
                     <>&nbsp;<a href={"http://github.com/" + props.github} title='GitHub'>
-                        <FontAwesomeIcon icon={faGithub} className='fa-xl'/></a>&nbsp;</>}
+                        <FontAwesomeIcon icon={faGithub} className='fa-xl' /></a>&nbsp;</>}
                 {props.telegram &&
                     <>&nbsp;<a href={"https://t.me/" + props.telegram} title='Telegram'>
-                        <FontAwesomeIcon icon={faTelegram} className='fa-xl'/></a>&nbsp;</>}
+                        <FontAwesomeIcon icon={faTelegram} className='fa-xl' /></a>&nbsp;</>}
+                {props.linkedin &&
+                    <>&nbsp;<a href={"https://www.linkedin.com/in/" + props.linkedin} title='LinkedIn'>
+                        <FontAwesomeIcon icon={faLinkedin} className='fa-xl' /></a>&nbsp;</>}
                 {props.twitter &&
                     <>&nbsp;<a href={"https://twitter.com/" + props.twitter} title='Twitter'>
-                        <FontAwesomeIcon icon={faTwitter} className='fa-xl'/></a>&nbsp;</>}
+                        <FontAwesomeIcon icon={faTwitter} className='fa-xl' /></a>&nbsp;</>}
                 &nbsp;
             </div>
         </div>
--- src/data/persons.js
+++ src/data/persons.js
@@ -7,6 +7,7 @@ const people = [
         github: 'fulcorno',
         twitter: 'fulcorno',
         telegram: 'fulcorno',
+        linkedin: 'fulviocorno',
     },
     {
         name: 'Laura Farinetti',
@@ -19,7 +20,8 @@ const people = [
         role: 'Associate Professor',
         email: 'luigi.derussis@polito.it',
         twitter: 'luigidr',
-        github: 'luigidr'
+        github: 'luigidr',
+        linkedin: 'luigidr',
     },
     {
         name: 'Alberto Monge Roffarello',
@@ -34,34 +36,141 @@ const people = [
         email: 'juan.saenz@polito.it',
         img: '/img/people/JuanSaenz.png',
         github: 'jpsaenzmo',
+        linkedin: 'jpsaenz',
     },
     {
         name: 'Luca Mannella',
         role: 'Ph.D. Student',
         email: 'luca.mannella@polito.it',
         img: '/img/people/LucaMannella.jpg',
-        github: 'Sarcares'
+        github: 'Sarcares',
+        linkedin: 'lucamannella',
     },
     {
         name: 'Tommaso Calò',
         role: 'Ph.D. Student',
         email: 'tommaso.calo@polito.it',
         img: '/img/people/TommasoCalo.jpeg',
-        github: 'tommasocalo'
+        github: 'tommasocalo',
+        linkedin: 'tommaso-caló-20509512a',
     }
 ]
 
 const alumni = [
     {
         name: 'Lorenzo Canale',
-        role: 'Ph.D. Graduate',
+        role: 'Artificial Intelligence Researcher at RAI - Radiotelevisione Italiana',
+        img: 'img/people/LorenzoCanale.jpeg',
+        linkedin: 'lorenzo-canale-a09436a7',
     },
     {
         name: 'Luisa Barrera León',
-        role: 'Ph.D. Graduate',
+        role: 'Post-Doc Researcher at Università degli Studi del Piemonte Orientale ',
         img: '/img/people/LuisaBarrera.jpg',
-        github: 'luifabelo'
+        github: 'luifabelo',
+        linkedin: 'luisabarreraleon',
     },
+    {
+        name: 'Fabio Ballati',
+        role: 'Ph.D. Student at Politecnico di Torino',
+        img: '/img/people/FabioBallati.webp',
+        // email: 'fabio.ballati@polito.it',
+        linkedin: 'fabio-ballati-290853ba',
+    },
+    {
+        name: 'Teodoro Montanaro',
+        role: 'Post-Doc Researcher at  Università del Salento',
+        img: '/img/people/TeodoroMontanaro.webp',
+        github: 'tmontanaro',
+        linkedin: 'teodoro-montanaro-94a50991',
+    },
+    {
+        name: 'Sebastián Aced López',
+        role: 'Computer Science Teacher and Robotic Instructor',
+        img: '/img/people/SebastianAcedLopez.webp',
+        linkedin: 'sebastián-aced-lópez-3a07b870',
+    },
+    {
+        name: 'Dario Bonino',
+        role: 'Senior Software Engineer at Sysdig',
+        img: '/img/people/DarioBonino.jpeg',
+        linkedin: 'dariobonino',
+        github: 'dbonino',
+    },
+    {
+        name: 'Muhammad Sanaullah',
+        role: 'Assistant Professor, Air University (Multan Campus)',
+        img: '/img/people/MuhammadSanaullah.jpeg',
+        linkedin: 'muhammad-sanaullah-dr-a3a4849',
+    },
+    {
+        name: 'Emiliano Castellina',
+        role: 'Insegnante di informatica presso IIS Avogadro Vercelli',
+        img: '/img/people/EmilianoCastellina.jpg',
+        linkedin: 'emiliano-castellina',
+    },
+    {
+        name: 'Faisal Razzak',
+        role: 'Senior Product Manager, R&D at Venafi Inc.',
+        img: '/img/people/FaisalRazzak.jpg',
+        linkedin: 'faisalrazzak',
+    },
+    {
+        name: 'Liu Min',
+    },
+    {
+        name: 'Alessandro Garbo',
+        role: 'Senior Software Engineering at Cerence Inc. ',
+        img: '/img/people/AlessandroGarbo.jpeg',
+        linkedin: 'alessandro-garbo-325506147',
+    },
+    {
+        name: 'Michele Debandi',
+        role: 'Developer, System integrator at Dedalus S.p.A.',
+        img: '/img/people/MicheleDebandi.jpeg',
+        linkedin: 'michele-debandi-9a43246',
+    },
+    {
+        name: 'Paolo Pellegrino',
+        role: ' Developer at Zirak srl',
+    },
+    {
+        name: 'Francesco Gerace',
+        role: 'IT Developer at Finance Evolution srl',
+        img: '/img/people/FrancescoGerace.jpg',
+    },
+    {
+        name: 'Alessio Bosca',
+        role: 'Research Development Software Engineer at H-Farm Innovation',
+        img: '/img/people/AlessioBosca.jpg',
+        linkedin: 'alessio-bosca',
+    },
+    {
+        name: 'Federico Pescarmona',
+        role: 'Software development engineer at Apple',
+        img: '/img/people/FedericoPescarmona.jpg',
+        linkedin: 'federicopescarmona',
+    },
+    {
+        name: 'Florin Bota',
+        role: 'Head of Sector - Data and Content Management - Operations and Security at European Commission',
+        img: '/img/people/FlorinBota.jpg',
+        linkedin: 'florin-bota-903b435',
+    },
+    {
+        name: 'Marco Luca Sbodio',
+        role: 'Senior Research Scientist at IBM Research',
+        img: '/img/people/MarcoSbodio.jpg',
+        linkedin: 'sbodio',
+    },
+    {
+        name: 'Franco Borello',
+        role: 'High School Teacher',
+        img: '/img/people/FrancoBorello.jpg',
+    },
+
+
+
 ]
 
 export {people, alumni};
