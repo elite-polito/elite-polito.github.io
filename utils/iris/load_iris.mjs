33147e2d76b56a2695d4637a8407b43ec041781f
Luigi De Russis <luigidr@gmail.com>
2024-07-11 16:42:32 +0200

    update the script to download the publications

--- utils/iris/load_iris.mjs
+++ utils/iris/load_iris.mjs
@@ -1,20 +1,27 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 
 import { allMembers } from '../../src/data/IRISAuthors.mjs';
 
-const baseURL = "https://www.swas.polito.it/dotnet/WMHandler/IrisEsportaJson.ashx?m="
-const base_install_directory = "./utils/iris"
-const default_output_directory = base_install_directory + "/cached_js"
+const baseURL = "https://www.swas.polito.it/dotnet/WMHandler/IrisEsportaJson.ashx?m=";
+const base_install_directory = "./utils/iris"; // to update according to the caller's path
+const default_output_directory = base_install_directory + "/cached_js";
+const final_output_directory = "./static/iris"; // to update according to the caller's path
 
 for (const author of allMembers) {
-    console.log(author);
 
     if (author.active) {
+        console.log(`Downloading ${author.first} ${author.last}'s publications...`);
         const response = await fetch(baseURL + author.id);
         const json = await response.json();
 
-        fs.writeFile(`${default_output_directory}/${author.id}.json`, JSON.stringify(json), (err) => {
-            if (err) console.error(err);
-        });
+        await fs.writeFile(`${default_output_directory}/${author.id}.json`, JSON.stringify(json));
+
+        console.log(`${author.first} ${author.last} has ${json['NumRec']} papers`);
+    }
+    else {
+        console.log(`Skipping ${author.first} ${author.last}'s publications.`);
     }
 }
+
+// move all the downloaded files to the 'static' folder of docusaurus
+fs.cp(`${default_output_directory}`, `${final_output_directory}`, {recursive: true});
